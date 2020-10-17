a934936f3a175998c4cc4b72f68be30c2befdbf6
HyeonChan Lee <hyeonchanlee@gmail.com>
2020-10-16 23:45:18 -0400

    CORS config

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 // CORS
 const whitelist = [
     'http://localhost:3000', 
-    'http://hyeonchanlee.github.io'
+    'https://hyeonchanlee.github.io'
 ];
 const corsOptions = {
     origin: (origin, callback) => {
