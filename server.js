28e5ed165df52267852e40f2f293b07c80e76d77
HyeonChan Lee <hyeonchanlee@gmail.com>
2020-11-09 22:27:43 -0500

    Debugging heroku

--- server.js
+++ server.js
@@ -49,14 +49,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Origin', req.headers.origin);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-//     next();
-// });
-
 // Bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -73,7 +65,7 @@ app.use(session({
     cookie: {
         maxAge: (14 * 24 * 60 * 60 * 1000), 
         httpOnly: true, 
-        sameSite: 'none', 
+        sameSite: 'lax', 
         secure: process.env.NPM_CONFIG_PRODUCTION
     }, 
     store: new MongoStore({ 
